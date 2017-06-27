9b91caf6bdd6e1a107a082b3f9b7eb5091ea0080
Barry <barry@netfiremedia.co.uk>
2017-06-27 20:18:34 +0100

    added netfire theme with working build stack

---
+++ wp-content/themes/netfire-child/gulpfile.js
@@ -0,0 +1,30 @@
+/**
+ * Created by barrypoore on 06/10/2016.
+ */
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sassGlob = require('gulp-sass-glob'),
+    cleanCSS = require('gulp-clean-css'),
+    sourcemaps = require('gulp-sourcemaps');
+
+
+gulp.task('sass', function () {
+    return gulp.src('sass/**/*.scss')
+        .pipe(sassGlob())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('sass/**/*.scss', ['sass']);
+});
+
+
+gulp.task('build', function() {
+    return gulp.src('style.css')
+        .pipe(cleanCSS({compatibility: 'ie9'}))
+        .pipe(gulp.dest('css/dist'));
+});
